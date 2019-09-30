55fd6c9653ca0ccece155fb37d93668d1dea8169
github@posmasterus.com <Netreca1978*>
2019-09-30 02:47:31 -0700

    commit

--- aerostartech_doc/public/config.php
+++ aerostartech_doc/public/config.php
@@ -1,11 +1,10 @@
-
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_SERVER', 'azdatatech.com');
+define('DB_USERNAME', 'yhpark11_aero');
+define('DB_PASSWORD', 'fish1234');
+define('DB_NAME', 'yhpark11_cms_doc');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
