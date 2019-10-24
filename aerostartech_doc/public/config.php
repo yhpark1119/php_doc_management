735032562f1950c6bdf4a249ab57a576a6a459b8
github@posmasterus.com <Netreca1978*>
2019-10-24 14:56:53 -0700

    aaaa

--- aerostartech_doc/public/config.php
+++ aerostartech_doc/public/config.php
@@ -1,10 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'azdatatech.com');
-define('DB_USERNAME', 'yhpark11_aero');
-define('DB_PASSWORD', 'fish1234');
-define('DB_NAME', 'yhpark11_cms_doc');
+
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
